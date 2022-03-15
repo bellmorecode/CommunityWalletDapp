7eaedc314a7b920f1a70db8df33e614f951b2375
Glenn Ferrie <glenn@gfdata.io>
2022-03-15 11:25:40 -0400

    updates to readme

--- community-wallet-dapp/src/App.js
+++ community-wallet-dapp/src/App.js
@@ -1,23 +1,36 @@
+import { BigNumber } from 'bignumber.js';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+const wei_per_eth = new BigNumber(10).pow(18);
 function App() {
+  const [currentAccount, setCurrentAccount] = useState(null);
+  
+  const getCommunityWalletDetails = () => {
+    return [{
+      wallet: "0xB7f34dcD629989B529aBb39d2ad7a9CFc1B653D5",
+      goal: 2 * wei_per_eth
+    }];
+  }
+
+  useEffect(() => {
+    let walletInfo = getCommunityWalletDetails();
+    console.log({walletInfo});
+    if (1===2) {
+      setCurrentAccount("foo");
+    }
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="nav">
+        <span>Community Wallet App</span>      
       </header>
+
+      <h1>Welcome to the Community Wallet Site!</h1>
+
+      {currentAccount ? <div>{currentAccount}</div> : <div>not connected!</div> }
+
     </div>
   );
 }
